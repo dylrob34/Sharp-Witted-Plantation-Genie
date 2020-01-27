53cd88d1284b27fd9a86e9d5c4f1d53a8702656d
dylrob34 <dylana1998@gmail.com>
2020-01-27 13:33:14 -0500

    test arduino script added

--- test/test.ino
+++ test/test.ino
@@ -3,26 +3,26 @@ SoftwareSerial mySerial(2, 3); // RX, TX
 
 //////////////////////////////////////////////////////////////////////////
 void OK_config()
-{  
- while(1)
+{
+  while (1)
   {
-char OK[2]="";
-   OK[0]=mySerial.read();
-   if(OK[0]=='O')
-     {
-      OK[1]=mySerial.read();
-      if(OK[1]=='K')
-        {
+    char OK[2] = "";
+    OK[0] = mySerial.read();
+    if (OK[0] == 'O')
+    {
+      OK[1] = mySerial.read();
+      if (OK[1] == 'K')
+      {
         break;
-        }
-     }
-   delay(20);
-   }  
+      }
+    }
+    delay(20);
+  }
 }
 //////////////////////////////////////////////////////////////////////////
 void esp8266_config()
 {
-  Serial.println("AT+RST");   
+  Serial.println("AT+RST");
   delay(1000);
   mySerial.println("AT+CWMODE_DEF=3");   //set the current Wi-Fi mode and save to Flash
   OK_config();
@@ -33,25 +33,25 @@ void esp8266_config()
   mySerial.println("AT+CIPSERVER=1");    //build TCP Server
   OK_config();
   Serial.println("Initialize the server!");
-  digitalWrite(13,HIGH);
+  digitalWrite(13, HIGH);
   OK_config();                          //wait Client to send OK; access to Client
 }
 ///////////////////////////////////////////////////////////////////////
-void setup() 
-{ 
+void setup()
+{
   Serial.begin(115200);
   mySerial.begin(115200);
-  pinMode(13,OUTPUT);
-  digitalWrite(13,LOW);
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
   esp8266_config();
 }
 void loop()
 {
- mySerial.println("AT+CIPSEND=0,9"); 
- delay(1000);
- mySerial.println("arduino"); 
- digitalWrite(13,LOW);
- delay(1000);
- digitalWrite(13,HIGH);
- delay(1000);
+  mySerial.println("AT+CIPSEND=0,9");
+  delay(1000);
+  mySerial.println("arduino");
+  digitalWrite(13, LOW);
+  delay(1000);
+  digitalWrite(13, HIGH);
+  delay(1000);
 }
