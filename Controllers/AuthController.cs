5919e86c689e4282530115febf6f8c955895e89c
Domenico Manna <mannadomenico2849@gmail.com>
2020-01-29 17:40:15 -0500

    Fix login route to accept unauthorized requests

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PlantationGenie.sendes;
@@ -29,6 +30,7 @@ namespace PlantationGenie.Controllers
 
         // auth/login
         [HttpPost("login")]
+        [AllowAnonymous]
         public TokenResponseObject PostLogin(AuthObject values)
         {
             string token = _authenticator.Authenticate(values.username, values.password);
